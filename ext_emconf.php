a29b9c501a65dd4ebd1a79780fb5fd75517b3520
Andreas Teumer <andreas.teumer@dataport.de>
2025-11-05 14:31:34 +0100

    chore: bump version to 4.3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,12 +2,12 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Container-Aware TOC DataProcessor',
-    'description' => 'Ready-to-use Table of Contents Content Element for TYPO3 v13 with Bootstrap 5, Kern UX, and full b13/container support. 3 layouts (sidebar/inline/dropdown), FlexForm config, WCAG 2.1 AA, Clean Architecture.',
+    'description' => 'Ready-to-use Table of Contents Content Element for TYPO3 v13 with Bootstrap 5, Kern UX, and full b13/container support. 3 layouts (sidebar/inline/compact), FlexForm config, WCAG 2.1 AA, Clean Architecture, zero dependencies.',
     'category' => 'fe',
     'author' => 'Andreas Teumer',
     'author_email' => 'aeaeue@ndrstmr.de',
     'state' => 'stable',
-    'version' => '4.2.0',
+    'version' => '4.3.0',
     'constraints' => [
         'depends' => [
             'typo3' => '13.4.0-13.99.99',
